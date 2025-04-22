db5830531853380c48854c06dd3ae009a53ecc4c
csaim11 <moz2130044@maricopa.edu>
2025-04-22 15:47:19 -0700

    Project 5 - Sorting Things Out

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -11,20 +11,20 @@ public class SortingUtility {
        achieve a sorting of Comparable objects
     */
 
-    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+    public static <T extends Comparable<T>> void gnomeSort(T[] a) {
 
         // TODO implement Gnome Sort here
 
         int pos = 0;
 
-        while (pos < data.length) {
-            if (pos == 0 || data[pos].compareTo(data[pos - 1]) >= 0) {
+        while (pos < a.length) {
+            if (pos == 0 || a[pos].compareTo(a[pos - 1]) >= 0) {
                 pos = pos + 1;
             } else {
                 // swap a[pos] and a[pos - 1]
-                T temp = data[pos];
-                data[pos] = data[pos - 1];
-                data[pos - 1] = temp;
+                T temp = a[pos];
+                a[pos] = a[pos - 1];
+                a[pos - 1] = temp;
                 pos = pos - 1;
             }
         }
@@ -35,7 +35,7 @@ public class SortingUtility {
            type of bubble sort to sort Comparable objects
         */
 
-    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+    public static <T extends Comparable<T>> void cocktailShakerSort(T[] A) {
 
         // TODO implement Cocktail Shaker Sort here
 
@@ -45,12 +45,12 @@ public class SortingUtility {
             swapped = false;
 
             // Forward pass
-            for (int i = 0; i < data.length - 1; i++) {
-                if (data[i].compareTo(data[i + 1]) > 0) {
+            for (int i = 0; i < A.length - 1; i++) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
                     // Swap elements
-                    T temp = data[i];
-                    data[i] = data[i + 1];
-                    data[i + 1] = temp;
+                    T temp = A[i];
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
                     swapped = true;
                 }
             }
@@ -62,12 +62,12 @@ public class SortingUtility {
             swapped = false;
 
             // Backward pass
-            for (int i = data.length - 2; i >= 0; i--) {
-                if (data[i].compareTo(data[i + 1]) > 0) {
+            for (int i = A.length - 2; i >= 0; i--) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
                     // Swap elements
-                    T temp = data[i];
-                    data[i] = data[i + 1];
-                    data[i + 1] = temp;
+                    T temp = A[i];
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
                     swapped = true;
                 }
             }
@@ -86,28 +86,28 @@ public class SortingUtility {
     */
 
 
-    public static <T extends Comparable<T>> void shellSort(T[] data) {
+    public static <T extends Comparable<T>> void shellSort(T[] a) {
 
         // TODO implement Shell Sort here
 
         ArrayList<Integer> gaps = new ArrayList<>(Arrays.asList(701, 301, 132, 57, 23, 10, 4, 1));
-        int n = data.length;
+        int n = a.length;
 
         // Start with the largest gap and work down to a gap of 1
         for (int gap : gaps) {
             // Gapped insertion sort for this gap size
             for (int i = gap; i < n; i++) {
-                T temp = data[i];
+                T temp = a[i];
                 int j = i;
 
                 // Shift earlier gap-sorted elements up until correct location is found
-                while (j >= gap && data[j - gap].compareTo(temp) > 0) {
-                    data[j] = data[j - gap];
+                while (j >= gap && a[j - gap].compareTo(temp) > 0) {
+                    a[j] = a[j - gap];
                     j -= gap;
                 }
 
                 // Put temp (original a[i]) in its correct location
-                data[j] = temp;
+                a[j] = temp;
             }
         }
     }
